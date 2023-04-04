f8f2b68c70746674bd91dffa390bfafb5ac82991
YolkinEgor <yolkinegor@gmail.com>
2023-04-04 18:47:51 +0500

    Добавление форматирования времени в определённом часовом поясе.

--- utils/dates_processing.py
+++ utils/dates_processing.py
@@ -7,10 +7,17 @@ months = {
 }
 
 
+def format_hour_to_timestamp(hour: int, timestamp: int = 2):
+    __hour = hour + timestamp
+    if hour in [22, 23, 24]:
+        __hour = (hour - 24) + timestamp
+    return __hour
+
+
 def dates_to_text(__datetime: datetime.datetime) -> str:
     date = '%s %s %s года в ' % (__datetime.day,
                                  months[__datetime.month],
                                  __datetime.year)
-    time = '{:02}:{:02}'.format(__datetime.hour, __datetime.minute)
+    time = '{:02}:{:02}'.format(format_hour_to_timestamp(__datetime.hour), __datetime.minute)
     __datetime = date + time
     return __datetime
